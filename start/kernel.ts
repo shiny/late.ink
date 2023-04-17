a899baa9b752b884c512dfd3cb328e1169647955
DAI JIE <daijie@php.net>
2023-04-17 18:48:17 +0800

    feat: register auth middlewares

--- start/kernel.ts
+++ start/kernel.ts
@@ -41,4 +41,7 @@ Server.middleware.register([
 | Route.get('dashboard', 'UserController.dashboard').middleware('auth')
 |
 */
-Server.middleware.registerNamed({})
+Server.middleware.registerNamed({
+    auth: () => import('App/Middleware/Auth'),
+    silentAuth: () => import('App/Middleware/SilentAuth'),
+})
