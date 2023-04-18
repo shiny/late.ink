f85ccc7d3b2c59526de0f457643151092a566e96
DAI JIE <daijie@php.net>
2023-04-18 20:22:13 +0800

    chore: add path alias

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import eslint from "vite-plugin-eslint"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,9 @@ export default defineConfig({
         manifest: true,
         outDir: "../src/public",
     },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, './src')
+        }
+    }
 })
