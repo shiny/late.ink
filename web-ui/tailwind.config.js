7e49e8c131d5a217d81393514fa021523e0de034
DAI JIE <daijie@php.net>
2023-04-18 20:25:01 +0800

    chore: remove daisyui logs

--- web-ui/tailwind.config.js
+++ web-ui/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    // eslint-disable-next-line no-undef
+    plugins: [ require("daisyui") ],
+    daisyui: {
+        logs: false,
+    }
 }
 
