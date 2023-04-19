8d0320603127ace3dc5e3dd04b52a7982d821f6c
daijie <daijie@keqin.tech>
2023-04-19 09:16:14 +0000

    chore: support server api proxy

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import eslint from "vite-plugin-eslint"
 import path from "path"
 
+const env = loadEnv('development', process.cwd(), '')
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), eslint()],
@@ -13,6 +14,14 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, './src')
+        }
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: env.API_ENDPOINT,
+                changeOrigin: true
+            },
         }
     }
 })
