98fbfb0b41fee2b7ea920a92835cab8e1964cc1a
daijie <daijie@keqin.tech>
2023-04-19 09:23:27 +0000

    feat: init css styles

--- web-ui/tailwind.config.js
+++ web-ui/tailwind.config.js
@@ -7,10 +7,45 @@ export default {
     theme: {
         extend: {},
     },
+    darkMode: 'class',
     // eslint-disable-next-line no-undef
-    plugins: [ require("daisyui") ],
+    plugins: [require("@tailwindcss/typography"), require('daisyui')],
     daisyui: {
         logs: false,
+        themes: [
+            {
+                late: {
+                    'primary': '#65c3c8',
+                    'primary-focus': '#42b2b8',
+                    'primary-content': '#ffffff',
+                    'secondary': '#ef9fbc',
+                    'secondary-focus': '#e7739e',
+                    'secondary-content': '#ffffff',
+                    'accent': '#eeaf3a',
+                    'accent-focus': '#e09915',
+                    'accent-content': '#ffffff',
+                    'neutral': '#4E5969',
+                    'neutral-focus': '#6B7785',
+                    'neutral-content': '#ffffff',
+                    'base-100': '#faf7f5',
+                    'base-200': '#efeae6',
+                    'base-300': '#e7e2df',
+                    'base-content': '#261230',
+                    'info': '#1c92f2',
+                    'success': '#009485',
+                    'warning': '#ff9900',
+                    'error': '#ff5724',
+                    '--rounded-box': '0.2rem',
+                    '--rounded-btn': '0.2rem',
+                    '--rounded-badge': '0.2rem',
+                    '--animation-btn': '.05s',
+                    '--animation-input': '.05s',
+                    '--btn-text-case': 'uppercase',
+                    '--navbar-padding': '.2rem',
+                    '--border-btn': '1px'
+                }
+            }
+        ]
     }
 }
 
