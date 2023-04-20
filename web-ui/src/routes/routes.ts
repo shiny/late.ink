630ef93d118e27878d60c33c23f76629dced0597
daijie <daijie@keqin.tech>
2023-04-20 12:03:03 +0000

    build: add generateReactRoutes plugin

---
+++ web-ui/src/routes/routes.ts
@@ -0,0 +1,28 @@
+// render index.html for those urls
+import type { PluginOption } from "vite"
+
+const routes = [
+    "/",
+    "/login",
+    "/certificate",
+    "/certificate/create"
+]
+
+export default routes
+
+/**
+ * Rollup Plugin to generate routes.json in /public dir
+ * @returns 
+ */
+export function generateReactRoutes(): PluginOption {
+    return {
+        name: 'generate-react-routes',
+        generateBundle() {
+            this.emitFile({
+                type: 'asset',
+                fileName: 'routes.json',
+                source: JSON.stringify(routes)
+            })
+        },
+    }
+}
--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig, loadEnv, PluginOption } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import eslint from "vite-plugin-eslint"
 import path from "path"
+// @ts-ignore
+import { generateReactRoutes } from "./src/routes/routes"
 
 const env = loadEnv('development', process.cwd(), '')
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), eslint()],
+    plugins: [react(), eslint(), generateReactRoutes()],
     build: {
         manifest: true,
         outDir: "../src/public",
