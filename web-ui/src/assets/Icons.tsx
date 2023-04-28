841ce7f5112dbecff628491bd1175d1a7cd60425
daijie <daijie@keqin.tech>
2023-04-28 10:41:37 +0000

    feat: create IconChecked

--- web-ui/src/assets/Icons.tsx
+++ web-ui/src/assets/Icons.tsx
@@ -69,11 +69,21 @@ export const IconInfo = ({ className }: IconProps) => <svg
 </svg>
 
 export const IconAdd = ({ className }: IconProps) => <svg
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className={className}
-    >
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-    </svg>
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+
+export const IconChecked = ({ className }: IconProps) => <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+</svg>
