f018115101e418863da571888895f296f9267b75
daijie <daijie@keqin.tech>
2023-05-04 06:30:02 +0000

    feat: global json error response

--- src/app/Exceptions/Handler.ts
+++ src/app/Exceptions/Handler.ts
@@ -15,9 +15,19 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
     constructor() {
         super(Logger)
+    }
+
+    public async handle(error: any, ctx: HttpContextContract): Promise<any> {
+        if (ctx.request.accepts(['json'])) {
+            return [
+                { message: error.message }
+            ]
+        }
+        return super.handle(error, ctx)
     }
 }
