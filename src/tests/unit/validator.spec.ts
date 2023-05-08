3334193364a9b3293bc1dce82882ca7aa798902c
daijie <daijie@keqin.tech>
2023-05-08 08:49:08 +0000

    style: format spaces indent

--- src/tests/unit/validator.spec.ts
+++ src/tests/unit/validator.spec.ts
@@ -3,32 +3,32 @@ import { rules, schema, validator } from '@ioc:Adonis/Core/Validator'
 
 test.group('validator', () => {
 
-  const correctWorkspace = 1
-  const incorrectWorkspace = 2
+    const correctWorkspace = 1
+    const incorrectWorkspace = 2
 
-  const data = {
-    accountId: 1
-  }
+    const data = {
+        accountId: 1
+    }
     test("check workspaceId", async ({ assert }) => {
-      const result = await validator.validate({
-        schema: schema.create({
-          accountId: schema.number([
-            rules.workspaceId('user_workspaces', correctWorkspace)
-          ])
-        }),
-        data
-      })
-      assert.deepEqual(result, data)
-
-      await assert.rejects(async () => {
-        await validator.validate({
-          schema: schema.create({
-            accountId: schema.number([
-              rules.workspaceId('user_workspaces', incorrectWorkspace)
-            ])
-          }),
-          data
+        const result = await validator.validate({
+            schema: schema.create({
+                accountId: schema.number([
+                    rules.workspaceId('user_workspaces', correctWorkspace)
+                ])
+            }),
+            data
         })
-      })
+        assert.deepEqual(result, data)
+
+        await assert.rejects(async () => {
+            await validator.validate({
+                schema: schema.create({
+                    accountId: schema.number([
+                        rules.workspaceId('user_workspaces', incorrectWorkspace)
+                    ])
+                }),
+                data
+            })
+        })
     })
 })
