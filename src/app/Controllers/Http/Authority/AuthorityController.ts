ededda933d5a4506e65b1e3ed57696acb3190a89
daijie <daijie@keqin.tech>
2023-05-09 09:11:08 +0000

    style: remove unused var

--- src/app/Controllers/Http/Authority/AuthorityController.ts
+++ src/app/Controllers/Http/Authority/AuthorityController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Authority from 'App/Models/Authority'
 
 export default class AuthorityController {
-    public async index({ workspaceId }: HttpContextContract) {
+    public async index({ }: HttpContextContract) {
         return await Authority.query().where({
             type: 'production'
         })
