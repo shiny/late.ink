c140fd8188fed743226c9bb4a807860a351f2550
daijie <daijie@keqin.tech>
2023-05-12 13:50:15 +0000

    feat: add IconCloud

--- web-ui/src/assets/Icons.tsx
+++ web-ui/src/assets/Icons.tsx
@@ -142,3 +142,14 @@ export const IconLogout = ({ className }: IconProps) => <svg
 >
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
 </svg>
+
+export const IconCloud = ({ className }: IconProps) => <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={`${className}`}
+>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z" />
+</svg>
