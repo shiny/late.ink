f2146d9224829264e3bc86f75fea6c585eb32bb5
daijie <daijie@keqin.tech>
2023-05-17 15:31:49 +0000

    fix(middleware): ensure the existence of  workspaceId

--- src/app/Middleware/Workspace.ts
+++ src/app/Middleware/Workspace.ts
@@ -5,6 +5,14 @@ export default class Workspace {
         ctx: HttpContextContract,
         next: () => Promise<void>
     ) {
+        if (!ctx.session.has('workspace_id')) {
+            if(ctx.auth.isAuthenticated) {
+                await ctx.auth.logout()
+                return ctx.response.unauthorized({
+                    error: 'unauthorized'
+                })
+            }
+        }
         ctx.workspaceId = ctx.session.get('workspace_id')
         await next()
     }
