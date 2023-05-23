9fa7a4a8667774961c788aa495bc90552595ab99
daijie <daijie@keqin.tech>
2023-05-23 11:13:38 +0000

    feat: add debounce helper

--- web-ui/src/utils/helper.ts
+++ web-ui/src/utils/helper.ts
@@ -16,3 +16,27 @@ export function arrayRange(start: number, end?: number, step = 1) {
     }
     return output
 }
+
+type debounceFn<T extends (...args: any[]) => any> = ((...args: Parameters<T>) => void) & {
+    cancel: () => void
+}
+
+export function debounce<T extends (...args: any[]) => any>(fn: T, wait: number): debounceFn<T> {
+    let timeoutId: ReturnType<typeof setTimeout> | undefined
+    const debouncedFn = function (this: any, ...args: Parameters<T>) {
+        const self = this
+        const later = function () {
+            timeoutId = undefined
+            fn.apply(self, args)
+        }
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(later, wait)
+    } as debounceFn<T>
+
+    debouncedFn.cancel = function () {
+        clearTimeout(timeoutId)
+        timeoutId = undefined
+    }
+
+    return debouncedFn
+}
