4a03bbd55576d24c829de80fd316fd7105928377
daijie <daijie@keqin.tech>
2023-05-31 14:00:31 +0000

    feat: use post to trigger a deployment

--- src/start/routes/deployment.ts
+++ src/start/routes/deployment.ts
@@ -3,7 +3,7 @@ import Route from "@ioc:Adonis/Core/Route"
 Route.group(() => {
     Route.resource('/deployment', 'Deployment/IndexController')
     Route.post('/deployment/validate', 'Deployment/IndexController.validate')
-    Route.get('/deployment/:id/trigger', 'Deployment/IndexController.trigger')
+    Route.post('/deployment/:id/trigger', 'Deployment/IndexController.trigger')
 })
 .prefix('/api/v1')
 .middleware('auth:web')
