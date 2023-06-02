ea4a5e0d2886207a7c98e97b8b7366b59bf9e081
daijie <daijie@keqin.tech>
2023-06-02 08:49:15 +0000

    pref(database): add index for job.name

--- src/database/migrations/1684932222277_jobs.ts
+++ src/database/migrations/1684932222277_jobs.ts
@@ -8,7 +8,7 @@ export default class extends BaseSchema {
     public async up() {
         this.schema.createTable(this.tableName, (table) => {
             table.increments('id')
-            table.string('name')
+            table.string('name').index()
             table.text('data')
             table.integer('outer_id').index()
             table.integer('workspace_id').index()
